58ff52454b61f893bdd123ddf22176f9ba9dc1a1
Lumaku <lumaku@users.noreply.github.com>
2021-06-04 23:16:57 +0200

    Refactor and re-do partitioning and timing in align.py

--- proof_of_concept_partitioning.py
+++ proof_of_concept_partitioning.py
@@ -7,62 +7,15 @@ import soundfile
 from pathlib import Path
 from espnet2.bin.asr_align import CTCSegmentation
 
+# from files in the same directory
+# make sure these files are in your PYTHONPATH
+from align import get_partitions
+
 # load model
 logging.basicConfig(
     level="INFO",
     format="%(asctime)s (%(module)s:%(lineno)d) %(levelname)s: %(message)s",
 )
-
-
-def get_partitions(
-    t=100000,
-    max_len_s=1280.0,
-    fs=16000,
-    samples_to_frames_ratio=512,
-    overlap=0,
-):
-    """Obtain partitions
-
-    Note that this is implemented for frontends that discard trailing data.
-    """
-    # max length should be ~ cut length + 25%
-    cut_time_s = max_len_s / 1.25
-    max_length = int(max_len_s * fs)
-    cut_length = int(cut_time_s * fs)
-    assert type(samples_to_frames_ratio) == int
-    # make sure its a multiple of frame length
-    max_length -= max_length % samples_to_frames_ratio
-    cut_length -= max_length % samples_to_frames_ratio
-    assert type(max_length) == int
-    assert type(cut_length) == int
-    overlap = int(max(0, overlap))
-    if (max_length - cut_length) <= samples_to_frames_ratio * (2 + overlap):
-        raise ValueError("Pick a larger time value for partitions.")
-    partitions = []
-    duplicate_frames = []
-    cumulative_lpz_length = 0
-    start = 0
-    while t > max_length:
-        t -= cut_length
-        end = start + cut_length + samples_to_frames_ratio * (1 + overlap)
-        partitions += [(start, end)]
-        # overlap
-        cumulative_lpz_length += (end - start) // samples_to_frames_ratio
-        for i in range(overlap):
-            duplicate_frames += [
-                cumulative_lpz_length - i,
-                cumulative_lpz_length + (1 + i),
-            ]
-        # start of next segment
-        start += cut_length - samples_to_frames_ratio * overlap
-    else:
-        partitions += [(start, None)]
-    partition_dict = {
-        "partitions": partitions,
-        "overlap": overlap,
-        "delete_overlap_list": duplicate_frames,
-    }
-    return partition_dict
 
 
 ## CONFIGURATION
@@ -75,14 +28,12 @@ text = (
     " ON PROPERTY MANAGEMENT".split()
 )
 
-
 d = ModelDownloader(cachedir="./modelcache")
-wsjmodel = d.download_and_unpack("kamo-naoyuki/wsj")
+model = d.download_and_unpack("kamo-naoyuki/wsj")
+
 speech, rate = soundfile.read(wav)
 
-aligner = CTCSegmentation(
-    **wsjmodel, fs=rate, time_stamps="fixed", kaldi_style_text=False
-)
+aligner = CTCSegmentation(**model, fs=rate, time_stamps="fixed", kaldi_style_text=False)
 aligner.set_config(samples_to_frames_ratio=aligner.estimate_samples_to_frames_ratio())
 # estimated index to frames ratio, usually 512, but sometimes 768
 # - depends on architecture
@@ -100,7 +51,9 @@ print(segments)
 name = "test"
 speech_len = speech.shape[0]
 speech = torch.tensor(speech)
-partitions = get_partitions(speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS)["partitions"]
+partitions = get_partitions(speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS)[
+    "partitions"
+]
 lpzs = [torch.tensor(aligner.get_lpz(speech[start:end])) for start, end in partitions]
 lpz = torch.cat(lpzs).numpy()
 task = aligner.prepare_segmentation_task(text, lpz, name, speech_len)
@@ -110,12 +63,13 @@ print(f"splitted into {LONGEST_AUDIO_SEGMENTS}s:")
 print(task)
 
 # make sure output has the same length
-assert segments.lpz.shape[0] == task.lpz.shape[0]
+if not segments.lpz.shape[0] == task.lpz.shape[0]:
+    print(f"ERROR: exp. {segments.lpz.shape[0]} != got {task.lpz.shape[0]} (task)")
 
 
 #####  Task_min uses audio that is splitted in 1s parts (shortest possible length for splitting)
 # "task_min" with min t for splitting
-partitions = get_partitions(speech.shape[0], max_len_s=1.00)["partitions"]
+partitions = get_partitions(speech.shape[0], max_len_s=1.05)["partitions"]
 lpzs = [torch.tensor(aligner.get_lpz(speech[start:end])) for start, end in partitions]
 lpz = torch.cat(lpzs).numpy()
 task_min = aligner.prepare_segmentation_task(text, lpz, name, speech_len)
@@ -125,12 +79,20 @@ print("splitted into smallest parts (min. t ~1s):")
 print(task_min)
 
 # make sure output has the same length
-assert segments.lpz.shape[0] == task_min.lpz.shape[0]
+if not segments.lpz.shape[0] == task_min.lpz.shape[0]:
+    print(
+        f"ERROR: exp. {segments.lpz.shape[0]} != got {task_min.lpz.shape[0]} (task_min)"
+    )
 
 ###### task_overlap is also split into 3s parts but additionally includes a little bit more audio
 ###### on the intersections between parts; also, the excess lpz indices are deleted.
-partitions = get_partitions(speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS, overlap=1)
-lpzs = [torch.tensor(aligner.get_lpz(speech[start:end])) for start, end in partitions["partitions"]]
+partitions = get_partitions(
+    speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS, overlap=1
+)
+lpzs = [
+    torch.tensor(aligner.get_lpz(speech[start:end]))
+    for start, end in partitions["partitions"]
+]
 lpz = torch.cat(lpzs).numpy()
 lpz = np.delete(lpz, partitions["delete_overlap_list"], axis=0)
 task_overlap = aligner.prepare_segmentation_task(text, lpz, name, speech_len)
@@ -140,14 +102,25 @@ print("With 1 overlapping lpz index:")
 print(task_overlap)
 
 # make sure output has the same length
-assert segments.lpz.shape[0] == task_overlap.lpz.shape[0]
+if not segments.lpz.shape[0] == task_overlap.lpz.shape[0]:
+    print(
+        f"ERROR: exp. {segments.lpz.shape[0]} != got {task_overlap.lpz.shape[0]} (task_overlap)"
+    )
 
 
 ###### task_overlap_n is the same as task_overlap, but now has N additional lpz indices
 OVERLAP = 10
-print(f"Overlap time: {512/16000*(2*OVERLAP)}s (overlap={OVERLAP})")
-partitions = get_partitions(speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS, overlap=OVERLAP)
-lpzs = [torch.tensor(aligner.get_lpz(speech[start:end])) for start, end in partitions["partitions"]]
+print(
+    f"Partitioning over {LONGEST_AUDIO_SEGMENTS}s."
+    f" Overlap time: {512/16000*(2*OVERLAP)}s (overlap={OVERLAP})"
+)
+partitions = get_partitions(
+    speech.shape[0], max_len_s=LONGEST_AUDIO_SEGMENTS, overlap=OVERLAP
+)
+lpzs = [
+    torch.tensor(aligner.get_lpz(speech[start:end]))
+    for start, end in partitions["partitions"]
+]
 lpz = torch.cat(lpzs).numpy()
 lpz = np.delete(lpz, partitions["delete_overlap_list"], axis=0)
 task_overlap_n = aligner.prepare_segmentation_task(text, lpz, name, speech_len)
@@ -157,7 +130,10 @@ print("With 2 overlapping indices:")
 print(task_overlap_n)
 
 # make sure output has the same length
-assert segments.lpz.shape[0] == task_overlap_n.lpz.shape[0]
+if not segments.lpz.shape[0] == task_overlap_n.lpz.shape[0]:
+    print(
+        f"ERROR: exp. {segments.lpz.shape[0]} != got {task_overlap_n.lpz.shape[0]} (task_overlap_n)"
+    )
 
 ##### plot activations
 # outdir = Path("../align-japanese-corpus/proof_of_concept_partitioning/")
